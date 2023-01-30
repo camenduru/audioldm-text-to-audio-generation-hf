a5d109b1d489f0b78e9da8e567bc72996929e8e1
haoheliu <867390095@qq.com>
2023-01-30 00:57:10 +0000

    add app file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import gradio as gr
 import numpy as np
 
 def greet(name):
-    return (16000, np.random.randn(16000))
+    return [(16000, np.random.randn(16000)), (16000, np.random.randn(16000)), (16000, np.random.randn(16000))]
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="audio")
+iface = gr.Interface(fn=greet, inputs="text", outputs=["audio", "audio", "audio"])
 iface.launch()
