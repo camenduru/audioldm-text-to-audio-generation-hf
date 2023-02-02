858c11b0a57579cfcd372d1d1238912977f05e6c
haoheliu <867390095@qq.com>
2023-02-02 00:32:07 +0000

    update

--- app.py
+++ app.py
@@ -11,53 +11,59 @@ def text2audio(text, duration, guidance_scale, random_seed):
     # waveform = [(16000, np.random.randn(16000)), (16000, np.random.randn(16000))]
     return waveform
 
-iface = gr.Interface(fn=text2audio, inputs=[
-        gr.Textbox(value="A man is speaking in a huge room", max_lines=1),
-        gr.Slider(2.5, 10, value=5, step=2.5),
-        gr.Slider(0, 5, value=2.5, step=0.5),
-        gr.Number(value=42)
-    ], outputs=[gr.Audio(label="Output", type="numpy"), gr.Audio(label="Output", type="numpy")]
-                     )
-iface.launch(share=True)
+# iface = gr.Interface(fn=text2audio, inputs=[
+#         gr.Textbox(value="A man is speaking in a huge room", max_lines=1),
+#         gr.Slider(2.5, 10, value=5, step=2.5),
+#         gr.Slider(0, 5, value=2.5, step=0.5),
+#         gr.Number(value=42)
+#     ], outputs=[gr.Audio(label="Output", type="numpy"), gr.Audio(label="Output", type="numpy")],
+#                 allow_flagging="never"
+#                      )
+# iface.launch(share=True)
 
-# block = gr.Blocks()
+iface = gr.Blocks()
 
-# with block:
-#     gr.HTML(
-#         """
-#             <div style="text-align: center; max-width: 700px; margin: 0 auto;">
-#               <div
-#                 style="
-#                   display: inline-flex;
-#                   align-items: center;
-#                   gap: 0.8rem;
-#                   font-size: 1.75rem;
-#                 "
-#               >
-#                 <h1 style="font-weight: 900; margin-bottom: 7px;">
-#                   Text-to-Audio Generation with AudioLDM
-#                 </h1>
-#               </div>
-#               <p style="margin-bottom: 10px; font-size: 94%">
-#                 <a href="https://arxiv.org/abs/2301.12503">[Paper]</a>  <a href="https://audioldm.github.io/">[Project page]</a>
-#               </p>
-#             </div>
-#         """
-#     )
-#     with gr.Group():
-#         with gr.Box():
-#             textbox = gr.Textbox(value="A man is speaking in a huge room")
-#             length = gr.Slider(1.0, 30.0, value=5.0, step=0.5, label="Audio length in seconds")
-#             # model = gr.Dropdown(choices=["harmonai/maestro-150k"], value="harmonai/maestro-150k",type="value", label="Model")
-#             out = [gr.Audio(label="Output", type="numpy"), gr.Audio(label="Output", type="numpy")]
-#             btn = gr.Button("Submit").style(full_width=True)
-        
-#         btn.click(text2audio, inputs=[textbox, length], outputs=out) 
-#         gr.HTML('''
-#         <div class="footer" style="text-align: center; max-width: 700px; margin: 0 auto;">
-#                     <p>Model by <a href="https://haoheliu.github.io/" style="text-decoration: underline;" target="_blank">Haohe Liu</a>
-#                     </p>
-#         </div>
-#         ''')
+with iface:
+    gr.HTML(
+        """
+            <div style="text-align: center; max-width: 700px; margin: 0 auto;">
+              <div
+                style="
+                  display: inline-flex;
+                  align-items: center;
+                  gap: 0.8rem;
+                  font-size: 1.75rem;
+                "
+              >
+                <h1 style="font-weight: 900; margin-bottom: 7px;">
+                  Text-to-Audio Generation with AudioLDM
+                </h1>
+              </div>
+              <p style="margin-bottom: 10px; font-size: 94%">
+                <a href="https://arxiv.org/abs/2301.12503">[Paper]</a>  <a href="https://audioldm.github.io/">[Project page]</a>
+              </p>
+            </div>
+        """
+    )
+    with gr.Group():
+        with gr.Box():
+            ############# Input
+            textbox = gr.Textbox(value="A hammer is hitting a wooden surface", max_lines=1)
+            duration = gr.Slider(2.5, 10, value=5, step=2.5)
+            guidance_scale = gr.Slider(0, 5, value=2.5, step=0.5)
+            seed = gr.Number(value=42)
+            ############# Output
+            outputs=[gr.Audio(label="Output", type="numpy"), gr.Audio(label="Output", type="numpy")]
+            
+            btn = gr.Button("Submit").style(full_width=True)
+        btn.click(text2audio, inputs=[textbox, duration, guidance_scale, seed], outputs=outputs) 
+        gr.HTML('''
+        <hr>
+        <div class="footer" style="text-align: center; max-width: 700px; margin: 0 auto;">
+                    <p>Model by <a href="https://haoheliu.github.io/" style="text-decoration: underline;" target="_blank">Haohe Liu</a>
+                    </p>
+        </div>
+        ''')
 
-# block.launch(debug=True)
+iface.queue(concurrency_count=2)
+iface.launch(debug=True, share=True)
